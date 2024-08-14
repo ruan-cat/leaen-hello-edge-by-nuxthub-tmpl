07de86be0f8df6c1fca8b3f820a828a447a7ca00
ruan-cat <1219043956@qq.com>
2024-08-14 09:10:19 +0800

    📃 docs: 标记，尝试在该项目内，实现边缘运行时连接其他的数据库；

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-30',
+  compatibilityDate: "2024-07-30",
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
 
   // https://nuxt.com/modules
-  modules: [
-    '@nuxthub/core',
-    '@nuxt/eslint'
-  ],
+  modules: ["@nuxthub/core", "@nuxt/eslint"],
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {},
@@ -17,19 +14,19 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: 'Hello from the Edge 👋'
-    }
+      helloText: "Hello from the Edge 👋",
+    },
   },
 
-  // https://eslint.nuxt.com
-  eslint: {
-    config: {
-      stylistic: {
-        quotes: 'single'
-      }
-    }
-  },
+  // // https://eslint.nuxt.com
+  // eslint: {
+  //   config: {
+  //     stylistic: {
+  //       quotes: 'single'
+  //     }
+  //   }
+  // },
 
   // https://devtools.nuxt.com
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+});
